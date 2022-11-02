91db2860fd15491e508f8f962e93e46d7f3f4f4f
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-11-02 11:25:37 +0900

    :tada: 코프링으로 변환
    야미~

---
+++ src/main/kotlin/com/g3c1/temiuser/TemiUserApplication.kt
@@ -0,0 +1,11 @@
+package com.g3c1.temiuser
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TemiUserApplication
+
+fun main(args: Array<String>) {
+	runApplication<TemiUserApplication>(*args)
+}
---
+++ src/test/kotlin/com/g3c1/temiuser/TemiUserApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.g3c1.temiuser
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TemiUserApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
