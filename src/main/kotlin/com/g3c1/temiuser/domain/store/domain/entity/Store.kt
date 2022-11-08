bc6a3ebadc89ffd07519be000b131db0c8ffb132
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-11-08 23:17:30 +0900

    :sparkles: entity 생성
    ㅇㅇ

--- src/main/kotlin/com/g3c1/temiuser/domain/store/domain/entity/Store.kt
+++ src/main/kotlin/com/g3c1/temiuser/domain/store/domain/entity/Store.kt
@@ -1,5 +1,15 @@
 package com.g3c1.temiuser.domain.store.domain.entity
 
-data class store(
-    val name: String
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
+data class Store(
+    @Id
+    @Column(name= "serial_number")
+    val serialNumber:Long,
+    val name: String,
+    val description: String,
+    val img: String
 )
