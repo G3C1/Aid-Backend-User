058153b86a605f8f62082aeafd43895b86827286
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-12-26 09:24:24 +0900

    :recycle: toResponse 변경
    좌표띠

--- src/main/kotlin/com/g3c1/temiuser/domain/seat/presentation/data/response/SeatInfoResponse.kt
+++ src/main/kotlin/com/g3c1/temiuser/domain/seat/presentation/data/response/SeatInfoResponse.kt
@@ -4,5 +4,11 @@ data class SeatInfoResponse(
     val seatId: Long,
     val seatNumber: Long,
     val severalPeople: Long,
+    val location: Location,
     val enabled: Boolean
-)
+) {
+    data class Location (
+        val x: Float,
+        val y: Float
+    )
+}
