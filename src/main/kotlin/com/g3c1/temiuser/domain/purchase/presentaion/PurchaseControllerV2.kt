86bb7a89335ab1b7347aa23467a5ba3fc340a99a
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-12-27 17:05:45 +0900

    :recycle: 값 변경
    ㅇㅇ

--- src/main/kotlin/com/g3c1/temiuser/domain/purchase/presentaion/PurchaseControllerV2.kt
+++ src/main/kotlin/com/g3c1/temiuser/domain/purchase/presentaion/PurchaseControllerV2.kt
@@ -9,9 +9,11 @@ import org.jetbrains.annotations.NotNull
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("api/v2/purchase")
@@ -19,8 +21,8 @@ class PurchaseControllerV2(
     private val purchaseServiceV2: PurchaseServiceV2,
     private val purchaseConverter: PurchaseConverter
 ) {
-    @GetMapping()
-    fun findPurchaseFoodListBySeatId(@RequestParam param: FindPurchasedFoodRequest):ResponseEntity<MyPurchasedFoodListResponse> =
+    @GetMapping
+    fun findPurchaseFoodListBySeatId(@RequestBody param: FindPurchasedFoodRequest):ResponseEntity<MyPurchasedFoodListResponse> =
         purchaseConverter.toDto(param)
             .let{purchaseServiceV2.findFoodListBySeatId(it)}
             .let { purchaseConverter.toResponse(it) }
--- src/main/kotlin/com/g3c1/temiuser/domain/purchase/presentaion/data/request/FindPurchasedFoodRequest.kt
+++ src/main/kotlin/com/g3c1/temiuser/domain/purchase/presentaion/data/request/FindPurchasedFoodRequest.kt
@@ -1,6 +1,10 @@
 package com.g3c1.temiuser.domain.purchase.presentaion.data.request
 
+import org.jetbrains.annotations.NotNull
+
 data class FindPurchasedFoodRequest (
+    @field: NotNull
     val seatId: Long,
+    @field: NotNull
     val serialNumber: Long
 )
