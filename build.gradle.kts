4575d6ce0c88eee9b3fbf3bd0609b28db8fd0018
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-12-30 09:14:45 +0900

    :heavy_plus_sign: 마리아 db로변경
    ㅇㅇ

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("com.mysql:mysql-connector-j")
